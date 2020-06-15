fb112638427f6c66fc8ba3cdb0ab796245d0798d
Biguit <leonardo.biguinatti@etec.sp.gov.br>
2020-06-15 16:25:34 -0300

    Inclusão Ex 5

---
+++ src/ex/pkg5/tp/pkg15/pkg06/pkg2020/pkg02/Ex5TP1506202002.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex.pkg5.tp.pkg15.pkg06.pkg2020.pkg02;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lucca
+ */
+public class Ex5TP1506202002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double N1, N2, NE, MD, ME;
+        
+    Scanner enter= new Scanner (System.in);
+    System.out.println ("Informe a nota 1: ");
+    N1= enter.nextDouble();
+    System.out.println ("Informe a nota 2: ");
+    N2= enter.nextDouble();
+    MD = (N1 + N2) / 2;
+    System.out.println ("A média do aluno é: "+ MD);
+    
+    if (MD >= 7)
+    {
+        System.out.println ("O aluno foi aprovado!");
+    }
+    else
+    {
+        System.out.println ("Digite sua nota do Exame: ");
+        NE= enter.nextDouble();
+        ME = (N1 + N2 + NE) / 3;
+        System.out.println ("Sua média do exame foi: "+ ME);
+        
+        
+            if (ME >= 5)
+            {
+                System.out.println ("O aluno foi aprovado em Exame!");
+            }
+
+            else
+            {
+                System.out.println ("O Aluno foi reprovado!");
+            }
+        
+    }
+        // TODO code application logic here
+    }
+    
+}
